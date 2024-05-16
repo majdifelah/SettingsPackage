4134302b7bdc7445e230ef55f90d3b284b001282
Majdi Felah <majdifelah@gmail.com>
2024-05-16 23:18:12 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SettingsPackage",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SettingsPackage",
+            targets: ["SettingsPackage"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SettingsPackage"),
+        .testTarget(
+            name: "SettingsPackageTests",
+            dependencies: ["SettingsPackage"]),
+    ]
+)
